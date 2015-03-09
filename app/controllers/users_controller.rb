a4ce4cbabb01d1b4de1429870877a82a18e57eda
CaseyKelly <casey.kelly@colorado.edu>
2015-03-09 11:52:54 -0600

    Add user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate, except: [:new, :create, :index]
+  before_action :authenticate, except: [:new, :create]
 
   def index
     @users = User.all
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to users_path, notice: "Thanks for signing up!"
+      redirect_to root_path, notice: "Thanks for signing up!"
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: "exceptions#index"
-  
+  resources :users
+
+  get '/login' => 'sessions#new', as: :login
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy', as: :logout
+  get '/signup' => 'users#new', as: :signup
+  post '/signup' => 'users#create'
 end
